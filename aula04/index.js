60f198085374a47610124feedb06655d6efab6d2
Jonas Diedrich <102824985+jonasdiedrich@users.noreply.github.com>
2022-06-22 20:24:23 -0300

    aula 04 e 05

--- aula04/index.js
+++ aula04/index.js
@@ -1,2 +1,11 @@
 // Estou escrevendo um comentario
 console.log("Hello world!");
+console.log("Linha 1");
+console.log("Linha 2");
+// Estou escrevendo outro comentario e ele nao ira interferir no console
+console.log("Linha 3");
+/*
+Para um comentario longo, com varias linhas..
+*/
+console.log("Linha 4"); // Estou escrevendo outro comentario e ele nao ira interferir no console
+console.log("Linha 5");
